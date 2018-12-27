78e0630aa2a24c42f513e40b0bb4ea14b88a7075
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2018-12-27 14:59:33 -0200

    ok

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,34 +1,39 @@
 <?php
 
-    require_once('db.class.php');
+	session_start();
 
-    $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
+	require_once('db.class.php');
+
+	$usuario = $_POST['usuario'];
+	$senha = md5($_POST['senha']);
+
+	$sql = " SELECT id, usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+
+	$objDb = new db();
+	$link = $objDb->conecta_mysql();
+
+	$resultado_id = mysqli_query($link, $sql);
+
+	if($resultado_id){
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+
+		if(isset($dados_usuario['usuario'])){
+
+			$_SESSION['id_usuario'] = $dados_usuario['id'];
+			$_SESSION['usuario'] = $dados_usuario['usuario'];
+			$_SESSION['email'] = $dados_usuario['email'];
+			
+			header('Location: home.php');
+
+		} else {
+			header('Location: index.php?erro=1');
+		}
+	} else {
+		echo 'Erro na execução da consulta, favor entrar em contato com o admin do site';
+	}
 
 
-
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
-
-
-    $objDb = new db();
-    $link = $objDb->conecta_mysql();
-
-
-    $resultado_id = mysqli_query($link,$sql);
-
-    if($resultado_id){
-        $dados_usuario = mysqli_fetch_array($resultado_id);
-
-
-        if(isset($dados_usuario['usuario'])){
-            echo 'existe';
-        } else {
-            echo 'nao existe';
-        }
-
-        } else{
-            echo 'Erro';
-        }
+	
 
 
 ?>
